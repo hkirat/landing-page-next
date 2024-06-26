2554536d00c3a9295b2c7aba550a6da6cf4bc625
harkirat <harkirat96@gmail.com>
2024-06-26 14:12:59 +0530

    Init

---
+++ app/api/pre-register/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function POST (request: NextRequest) {
+    const body = await request.json();
+    console.log(body);
+    return NextResponse.json({
+        message: "Done"
+    })
+}
